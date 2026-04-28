ff3bff9345737847e232b917bd518c35993dbca0
Josh Le Quang <130911256+KhoiNguyenLeQuang@users.noreply.github.com>
2026-04-28 10:08:07 -0400

    Update LCD.c

--- LCD.c
+++ LCD.c
@@ -2,81 +2,87 @@
 #include <msp430g2553.h> 
 
 void delay_us(unsigned int us) {
-    TA0CTL = TACLR;                 
-    TA0CCR0 = us;                   
-    TA0CTL = TASSEL_2 | MC_1;      
-    while ((TA0CTL & TAIFG) == 0);  
-    TA0CTL &= ~TAIFG;               
-    TA0CTL = MC_0;                
+    while (us--) {
+        __delay_cycles(1); 
+    }
 }
 
 void delay_ms(unsigned int ms) {
-    while (ms > 0) {
-        delay_us(1000); 
-        ms--;
+    while (ms--) {
+        __delay_cycles(1000); 
     }
 }
 
 void pulseEnable(void) {
-    CTRL_OUT |= E_PIN;        
-    delay_us(1);             
-    CTRL_OUT &= ~E_PIN;      
-    delay_ms(2);             
-}
-
-// 4-bit Data Function
-void sendNibble(unsigned char nibble) {
-    DATA_OUT &= 0x0F;                // Clear the upper 4 bits (P1.4 - P1.7)
-    DATA_OUT |= (nibble & 0xF0);     // Set the upper 4 bits with data
-    pulseEnable();
+    CTRL_OUT |= E_PIN;
+    __delay_cycles(1);
+    CTRL_OUT &= ~E_PIN;
+    __delay_cycles(50); 
 }
 
 void writeCommand(unsigned char command) {
-    CTRL_OUT &= ~RS_PIN;             // RS LOW for Command
-    sendNibble(command);             // Send High Nibble (Top 4 bits)
-    sendNibble(command << 4);        // Send Low Nibble (Bottom 4 bits shifted up)
+    CTRL_OUT &= ~RS_PIN;
+
+    DATA_OUT = command;  
+
+    pulseEnable();
+
+    if (command == 0x01 || command == 0x02) {
+        delay_ms(2);
+    }
 }
 
 void writeChar(char data) {
-    CTRL_OUT |= RS_PIN;              // RS HIGH for Text
-    sendNibble(data);                // Send High Nibble
-    sendNibble(data << 4);           // Send Low Nibble
+    CTRL_OUT |= RS_PIN;
+
+    DATA_OUT = data;
+
+    pulseEnable();
 }
 
-// Upgraded Print String with 16-character auto line-break
 void printString(char* text) {
-    unsigned char char_count = 0; 
+    unsigned char addr = 0;
+    unsigned int count = 0;
+
     while (*text != '\0') {
-        if (char_count == 16) {
-            writeCommand(0xC0);      // Jump to line 2
+
+        if (count == 16) {
+            writeCommand(0xC0); 
         }
-        if (char_count >= 32) {
-            break;                   // Stop printing if screen is full
+
+        if (count >= 32) {
+            break;
         }
+
         writeChar(*text);
-        char_count++;
         text++;
+        count++;
+    }
+}
+
+void printStringWrapped(char* text) {
+    unsigned int count = 0;
+
+    while (*text != '\0' && count < 32) {
+
+        if (count == 16) {
+            writeCommand(0xC0); 
+        }
+
+        writeChar(*text);
+        text++;
+        count++;
     }
 }
 
 void initLCD(void) {
-    DATA_DIR |= 0xF0;                 // Set only P1.4 to P1.7 as outputs
+    DATA_DIR = 0xFF;                  
     CTRL_DIR |= (RS_PIN | E_PIN);     
     CTRL_OUT &= ~(RS_PIN | E_PIN);
     
     delay_ms(20);
     
-    // Strict 4-bit wake-up sequence
-    CTRL_OUT &= ~RS_PIN;
-    sendNibble(0x30);
-    delay_ms(5);
-    sendNibble(0x30);
-    delay_ms(1);
-    sendNibble(0x30);
-    delay_ms(1);
-    sendNibble(0x20);        
-    
-    writeCommand(0x28);      // 4-bit mode, 2 lines, 5x8 font
+    writeCommand(0x38);      // 8-bit mode, 2 lines
     writeCommand(0x0C);      // Display ON, Cursor OFF
     writeCommand(0x01);      // Clear Screen
     writeCommand(0x06);      // Auto-increment cursor
@@ -84,21 +90,16 @@ void initLCD(void) {
 
 void clearLCD(void) {
     writeCommand(0x01);
+    delay_ms(2);
 }
 
-void setCursor(unsigned char row, unsigned char col) {
-    unsigned char address = (row == 0) ? 0x80 : 0xC0;
-    address |= col;
-    writeCommand(address);
-}
-
-// Timer Functions from Activity 24
 void printTime(unsigned long ticks) {
-    unsigned int mins = (ticks / 6000);       // Calculate minutes
-    unsigned int secs = (ticks / 100) % 60;   // Calculate seconds
-    unsigned int hund = ticks % 100;          // Calculate hundredths of a second
-    
+    unsigned int mins = ticks / 60000;
+    unsigned int secs = (ticks / 1000) % 60;
+    unsigned int hund = (ticks/10) % 100;
+
     char buffer[9];
+
     buffer[0] = (mins / 10) + '0';
     buffer[1] = (mins % 10) + '0';
     buffer[2] = ':';
@@ -108,53 +109,47 @@ void printTime(unsigned long ticks) {
     buffer[6] = (hund / 10) + '0';
     buffer[7] = (hund % 10) + '0';
     buffer[8] = '\0';
-    
-    writeCommand(0x80); 
-    printString("Time:      ");
-    writeCommand(0xC0); 
+
+    writeCommand(0xC0 + 4);
     printString(buffer);
-    printString("   "); 
 }
 
 void printAvg(unsigned long ticks) {
-    unsigned int mins = (ticks / 6000);       
-    unsigned int secs = (ticks / 100) % 60;   
-    unsigned int hund = ticks % 100;          
-    
-    // Create a buffer exactly 16 characters long + 1 null terminator
-    char buffer[17]; 
-    
-    // Pad the front with 4 spaces
-    buffer[0] = ' '; buffer[1] = ' '; buffer[2] = ' '; buffer[3] = ' ';
-    
-    // Insert the 8-character time
-    buffer[4] = (mins / 10) + '0';
-    buffer[5] = (mins % 10) + '0';
-    buffer[6] = ':';
-    buffer[7] = (secs / 10) + '0';
-    buffer[8] = (secs % 10) + '0';
-    buffer[9] = '.';
-    buffer[10] = (hund / 10) + '0';
-    buffer[11] = (hund % 10) + '0';
-    
-    buffer[12] = ' '; buffer[13] = ' '; buffer[14] = ' '; buffer[15] = ' ';
-    buffer[16] = '\0';
-    
-    writeCommand(0x80); 
+    unsigned int mins = ticks / 60000;
+    unsigned int secs = (ticks / 1000) % 60;
+    unsigned int hund = (ticks/10) % 100;
+
+    char buffer[9];
+
+    buffer[0] = (mins / 10) + '0';
+    buffer[1] = (mins % 10) + '0';
+    buffer[2] = ':';
+    buffer[3] = (secs / 10) + '0';
+    buffer[4] = (secs % 10) + '0';
+    buffer[5] = '.';
+    buffer[6] = (hund / 10) + '0';
+    buffer[7] = (hund % 10) + '0';
+    buffer[8] = '\0';
+
+    writeCommand(0x80);
     printString("  Final Average ");
-    
-    writeCommand(0xC0); 
-    printString(buffer);             
+
+    writeCommand(0xC0 + 4);
+    printString(buffer);
 }
+
 void printError(unsigned long pct) {
-    char buf[17] = "      000%      ";
-        
-    buf[6] = (pct / 100) + '0';
-    buf[7] = ((pct / 10) % 10) + '0';
-    buf[8] = (pct % 10) + '0';
-        
+    char buf[5];
+
+    buf[0] = (pct / 100) + '0';
+    buf[1] = ((pct / 10) % 10) + '0';
+    buf[2] = (pct % 10) + '0';
+    buf[3] = '%';
+    buf[4] = '\0';
+
     writeCommand(0x80);
     printString("   Avg Error:   ");
-    writeCommand(0xC0);
+
+    writeCommand(0xC0 + 6);
     printString(buf);
 }
